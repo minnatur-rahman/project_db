11c63c037b3ebf959e6bd95e640e2f514c0cbe31
millatur <minnatur7373@gmail.com>
2023-12-15 00:45:08 +0600

    my night commiot

--- database/migrations/2023_12_14_175716_create_tasts_table.php
+++ database/migrations/2023_12_14_175716_create_tasts_table.php
@@ -11,11 +11,9 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('students', function (Blueprint $table) {
+        Schema::create('tasts', function (Blueprint $table) {
             $table->id();
-            $table->string('name', 30);
-            $table->string('email');
-            $table->float('peacentage', 3, 2);
+            $table->timestamps();
         });
     }
 
@@ -24,6 +22,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('students');
+        Schema::dropIfExists('tasts');
     }
 };
