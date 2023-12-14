10ed1eb0e5f212d89b4e63b2dc0a590d374c3af1
millatur <minnatur7373@gmail.com>
2023-12-14 07:03:55 +0600

    db first and second commit

--- database/migrations/2023_12_14_005104_creat_students_table.php
+++ database/migrations/2023_12_14_005104_creat_students_table.php
@@ -11,7 +11,9 @@ return new class extends Migration
      */
     public function up(): void
     {
-        //
+        Schema::create('students', function(Blueprint $table){
+             $table->id();
+        });
     }
 
     /**
