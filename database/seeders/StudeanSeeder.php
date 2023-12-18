75603cfb63401d156b6326041b42797df71889f0
millatur <minnatur7373@gmail.com>
2023-12-19 05:47:42 +0600

    first commit me y

--- database/seeders/StudeanSeeder.php
+++ database/seeders/StudeanSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\studean;
 
 class StudeanSeeder extends Seeder
 {
@@ -12,6 +13,6 @@ class StudeanSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+
     }
 }
