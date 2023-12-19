f921578f9ca31f5b8091bb6e43548a9a33ee395b
millatur <minnatur7373@gmail.com>
2023-12-19 07:08:25 +0600

    sast commit

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
+
 class DatabaseSeeder extends Seeder
 {
     /**
@@ -12,6 +13,12 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
+        $this->call([
+            StudeanSeeder::class
+        ]);
+
+
+
         // \App\Models\User::factory(10)->create();
 
         // \App\Models\User::factory()->create([
--- database/seeders/StudeanSeeder.php
+++ database/seeders/StudeanSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use App\Models\studean;
+use Illuminate\Support\Facades\File;
 
 class StudeanSeeder extends Seeder
 {
@@ -14,5 +15,18 @@ class StudeanSeeder extends Seeder
     public function run(): void
     {
 
+        $jason = File::get(path:'database/json/students.json');
+
+        $students = collect(json_decode($jason));
+
+
+         $students->each(function($student){
+                 studean::insert($student);
+         });
+
+        // studean::create([
+        //     'name' => 'Minnatur Rahman',
+        //     'email' => 'minnatur@gmail.com'
+        // ]);
     }
 }
